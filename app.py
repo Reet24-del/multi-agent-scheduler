5f50146740cd49601f23c541dacb1b9042b5fea6
Reet Singh <reet2402singh@gmail.com>
2026-07-15 09:09:39 +0530

    Bump version to 1.1.6 to trigger Vercel build with DB_URL environment

--- app.py
+++ app.py
@@ -807,7 +807,7 @@ async def get_index():
 
 @app.get("/version")
 async def get_version():
-    return {"version": "1.1.5"}
+    return {"version": "1.1.6"}
 
 @app.get("/env-keys")
 async def get_env_keys():
